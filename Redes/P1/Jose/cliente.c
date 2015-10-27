dc7781f5088143b1322f08844179016afe409044
José Pérez-Parras Toledano <i32petoj@uco.es>
2015-10-27 19:37:26 +0100

    Practica 1 subida

---
+++ Redes/P1/Jose/cliente.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+//Includes de las librerias para que funcione el cliente servidor
+
+int main(int argc, char ** argv){
+
+  struct sockaddr_in servidor;
+  int descriptor;
+  int envio, recibido=0;
+  char mensajeRec[100];
+  char mensaje[100];
+  struct timeval timeout;
+  int salida; //Salida del select
+  int contador=0;
+  fd_set lectura; //Para el select
+
+
+
+  //Le asignamos los datos al sockaddr
+  servidor.sin_family = AF_INET;
+  servidor.sin_addr.s_addr = inet_addr(argv[1]);
+  servidor.sin_port = htons(2000);
+  int longitud_Servidor = sizeof(servidor);
+
+  //Cogemos el descriptor
+  descriptor = socket(AF_INET, SOCK_DGRAM, 0);
+  if (descriptor == -1)
+  {
+    printf ("No se puede abrir socket servidor\n");
+    return (-1);
+  }
+  printf("Introduce la opcion: ");
+  scanf("%s", mensaje);
+
+
+  //Vamos a enviarle un mensaje al servidor
+
+
+
+  
+  while(contador<3 && recibido==0){
+  
+    envio = sendto (descriptor, &mensaje, sizeof(mensaje), 0, (struct sockaddr *) &servidor, longitud_Servidor);
+    if(envio < 1){
+      printf("Error al solicitar el servicio de envio\n");
+    }
+
+      FD_ZERO(&lectura);
+      FD_SET(descriptor, &lectura);
+
+      //Fijamos el timeout para el select
+      timeout.tv_sec = atoi(argv[2]);
+      timeout.tv_usec=0;
+
+    salida = select(descriptor+1, &lectura, NULL, NULL, &timeout); //Ponemos el select despues del envio, esperando la respuesta
+
+    if(salida ==-1){
+      printf("Se ha producido un error en el select\n");
+      return -1;
+    }else if(salida==0){
+      printf("Se ha agotado el tiempo\n");
+      contador++;
+    }else {
+        //Ahora esperamos la respuesta del server
+        recibido = recvfrom(descriptor, (char*)&mensajeRec, sizeof(mensajeRec), 0, (struct sockaddr*) &servidor, &longitud_Servidor);
+
+        if(recibido>0){
+          printf("%s\n", mensajeRec);
+        }else{
+          printf("Ningun mensaje de vuelta\n");
+        }
+      }
+
+   }
+
+   if(contador==3){
+    printf("Se han realizado 3 intentos\n");
+   }
+  close(descriptor);
+  return 0;
+}
---
+++ Redes/P1/Jose/servidor.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+//Includes de las librerias para que funcione el cliente servidor
+
+int main(){
+
+  struct sockaddr_in servidor;
+  struct sockaddr_in cliente;
+  char buffer[100];
+  char *mensaje;
+  mensaje = (char*)malloc(100*sizeof(char));
+  int recibido;
+  time_t tiempo;
+  struct tm *stTm;
+
+  int longitud_cliente = sizeof(cliente);
+  int descriptor;
+
+  stTm = localtime(&tiempo);
+
+  descriptor = socket(AF_INET, SOCK_DGRAM, 0);
+  if (descriptor == -1)
+  {
+    printf ("No se puede abrir socket servidor\n");
+    return(-1);
+  }
+
+
+  //Le asignamos los datos al sockaddr
+  servidor.sin_family = AF_INET;
+  servidor.sin_addr.s_addr = htonl(INADDR_ANY);
+  servidor.sin_port = htons(2000);
+
+  if(bind(descriptor, (struct sockaddr *)&servidor, sizeof(servidor))==-1){
+    close(descriptor);
+    printf("Something went wrong\n");
+    return(-1);
+  }
+
+
+  //Hacemos el bucle para que el servidor este siempre escuchando Ü
+  while(1){
+    //Recibimos el mensaje del cliente
+    recibido = recvfrom(descriptor, (char *)&buffer, sizeof(buffer), 0, (struct sockaddr *) &cliente, &longitud_cliente);
+
+
+    if(recibido > 0){
+
+        if(strcmp(buffer, "DAY") == 0){
+
+              strftime(buffer,100,"%A, %d of %B of %Y", stTm);
+
+         }else if(strcmp(buffer, "DAYTIME") == 0){
+
+               strftime(buffer,80,"%A, %d of %B of %Y; %H:%M:%S", stTm);
+
+         }else if(strcmp(buffer, "TIME") == 0){
+
+                strftime(buffer,80,"%H:%M:%S", stTm);
+
+        }else{
+          strcpy(buffer, "No coincide");
+        }
+
+      //Aqui le enviaremos un mensaje al cliente si el servidor ha recibido algo
+      sendto (descriptor, &buffer, sizeof(buffer), 0, (struct sockaddr *) &cliente, longitud_cliente);
+      printf("Mensaje recibido\n");
+    }
+
+  }
+
+
+  //Al final de todo deberemos cerrar el socket
+  close(descriptor);
+}
