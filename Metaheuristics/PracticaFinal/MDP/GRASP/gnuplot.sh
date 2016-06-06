1768d0f2f4162d0329a59ada74d098bd423df0c9
i32petoj <i32petoj@uco.es>
2016-06-06 12:18:57 +0200

    Graficas del grasp

--- Metaheuristics/PracticaFinal/MDP/GRASP/gnuplot.sh
+++ Metaheuristics/PracticaFinal/MDP/GRASP/gnuplot.sh
@@ -2,9 +2,9 @@
 
 cat << _end_ | gnuplot
 set terminal postscript eps color
-set output "GRASP50.eps"
+set output "GRASP150.eps"
 set key right bottom
 set xlabel "Numero de iteraciones"
 set ylabel "Diversidad"
-plot 'prueba.txt' using 1:2 t "graspito" w l, 'prueba.txt' using 1:3 t "siguiente" w l
+plot 'grasp150_1.txt' using 1:2 t "GRASP" w l
 _end_
